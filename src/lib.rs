cc40fb3cdec61c2089eeff71f8c7417e254ea442
hyq <betterhyq@qq.com>
2026-02-22 00:08:59 +0800

    feat: add is_uppercase function to check if a character is uppercase

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
 use urlogger::{LogLevel, log};
 
+pub fn is_uppercase(c: char) -> Option<bool> {
+    if c.is_ascii_digit() {
+        return None;
+    }
+    let lower = c.to_lowercase().next().unwrap_or(c);
+    Some(c != lower)
+}
+
 pub fn hello(name: &str) -> String {
     log!(LogLevel::Info, "lib.rs");
     format!("Hello, {}!", name)
